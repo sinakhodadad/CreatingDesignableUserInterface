696e1677cba1a35b00e85b5cf3fd7c7b9184148e
sinahk <sinahk1992@yahoo.com>
2019-02-12 13:41:25 +0330

    Initial Commit

---
+++ CreatingDesignableUserInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreatingDesignableUserInterface
+//
+//  Created by sinahk on 2/12/19.
+//  Copyright © 2019 sinakhodadad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
